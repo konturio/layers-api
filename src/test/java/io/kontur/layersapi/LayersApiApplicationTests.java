f0045ecee3c4f255267d7a200ea30d73217d7fa1
Alexander Zapasnik <sanych27@gmail.com>
2021-12-17 13:29:16 +0300

    Initial commit

---
+++ src/test/java/io/kontur/layersapi/LayersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kontur.layersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayersApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
