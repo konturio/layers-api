54a94f549515edd4de40ddbac036beff259a7b8d
Alexander Zapasnik <sanych27@gmail.com>
2022-01-11 15:49:44 +0300

    Use user id instead of login name for layers ownership

--- src/main/java/io/kontur/layers/util/AuthorizationUtils.java
+++ src/main/java/io/kontur/layers/util/AuthorizationUtils.java
@@ -21,16 +21,13 @@ public class AuthorizationUtils {
         }
         switch (authentication.getPrincipal().getClass().getSimpleName()) {
             case "User":
-                return authentication.getName();
             case "Jwt":
-                Jwt principal = (Jwt) authentication.getPrincipal();
-                return (String) principal.getClaims().get("preferred_username");
+                return authentication.getName();
             default:
                 LOG.error(String.format("Can't authorize user with %s principal.",
                         authentication.getPrincipal().getClass().getName()));
                 throw new WebApplicationException(HttpStatus.INTERNAL_SERVER_ERROR, Error.error("Can't authorize"));
         }
-
     }
 
 }
