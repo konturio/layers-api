b29afa1a844317bd47371b6dab97d05fc4323611
Ivan Shilyaev <ishilyaev@kontur.io>
2022-10-19 15:53:25 +0300

    #12813 Add liquibase tags. Move changes to new release

--- src/main/resources/db/changelog/v1.9/create-smart-city-application-12813.sql
+++ src/main/resources/db/changelog/v1.9/create-smart-city-application-12813.sql
@@ -1,3 +1,5 @@
+--changeset layers-api-migrations:v1.9/create-smart-city-application-12813.sql runOnChange:false
+
 insert into apps (id, show_all_public_layers, is_public, owner)
 values ('634f23f5-f898-4098-a8bd-09eb7c1e1ae5', true, true, 'layers_db')
 ON CONFLICT (id) DO NOTHING;
