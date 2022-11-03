8554c882df9604b8eb50919a9338683e488e2585
avalasiuk <avalasiuk@kontur.io>
2022-11-03 12:55:40 +0300

    Remove removing of zoom_visibility_rools field

--- src/main/resources/db/changelog/v1.10/alter-layers-add-min-max-zoom.sql
+++ src/main/resources/db/changelog/v1.10/alter-layers-add-min-max-zoom.sql
@@ -1,13 +1,10 @@
 --liquibase formatted sql
 
---changeset layers-api-migrations:v1.10/alter-layers-add-min-max-zoom-rm-zoom-visibility-rools.sql runOnChange:false
+--changeset layers-api-migrations:v1.10/alter-layers-add-min-max-zoom.sql runOnChange:false
 
 ALTER TABLE layers
     ADD COLUMN IF NOT EXISTS min_zoom INTEGER,
     ADD COLUMN IF NOT EXISTS max_zoom INTEGER;
-
-ALTER TABLE layers
-    DROP COLUMN IF EXISTS zoom_visibility_rules;
 
 UPDATE TABLE layers
     SET min_zoom = null,
