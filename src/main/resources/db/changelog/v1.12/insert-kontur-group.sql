3e2247f9c438961c13df62ba828870145d7a6dc5
Alexander Zapasnik <sanych27@gmail.com>
2022-12-29 16:15:53 +0100

    #8040 Add Kontur group

--- src/main/resources/db/changelog/v1.12/insert-kontur-group.sql
+++ src/main/resources/db/changelog/v1.12/insert-kontur-group.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset layers-api-migrations:insert-layers-group.sql runOnChange:true
+--changeset layers-api-migrations:v1.12/insert-kontur-group.sql runOnChange:false
 
 INSERT INTO layers_group_properties (name, is_opened, mutually_exclusive)
 VALUES ('Kontur', true, true)
