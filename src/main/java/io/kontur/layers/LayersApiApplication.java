990fbf6ce6b306225d8667be612fb00865db653d
Alexander Kolesen <alexander.kolesen@gmail.com>
2024-02-18 23:15:47 +0100

    Sentry integration

--- src/main/java/io/kontur/layers/LayersApiApplication.java
+++ src/main/java/io/kontur/layers/LayersApiApplication.java
@@ -1,5 +1,6 @@
 package io.kontur.layers;
 
+import io.sentry.Sentry;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
