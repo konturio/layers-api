7bb5ab7ee4bab373994284ddd7858a5a0d1c6bce
Polina Krukovich <polina.krukovich7@gmail.com>
2024-08-13 10:05:18 +0200

    #14420 log incoming headers

--- src/main/java/io/kontur/layers/controller/DefaultApi.java
+++ src/main/java/io/kontur/layers/controller/DefaultApi.java
@@ -7,15 +7,20 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class DefaultApi {
 
     private final LandingPageService landingPageService;
+    private final Logger log = LoggerFactory.getLogger(DefaultApi.class);
 
     public DefaultApi(LandingPageService landingPageService) {
         this.landingPageService = landingPageService;
@@ -26,8 +31,17 @@ public class DefaultApi {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "The landing page provides links to the API definition (link relations `service-desc` and `service-doc`), the Conformance declaration (path `/conformance`, link relation `conformance`), and the Feature Collections (path `/collections`, link relation `data`).", content = @Content(schema = @Schema(implementation = LandingPage.class))),
             @ApiResponse(responseCode = "500", description = "A server error occurred.", content = @Content(schema = @Schema(implementation = Exception.class)))})
-    public ResponseEntity<LandingPage> getLandingPage() {
+    public ResponseEntity<LandingPage> getLandingPage(HttpServletRequest request) {
+        logRequestHeaders(request);
         return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON)
                 .body(landingPageService.getLandingPage());
     }
+
+    private void logRequestHeaders(HttpServletRequest request) {
+        log.info("X-Forwarded-For: {}", request.getHeader("X-Forwarded-For"));
+        log.info("X-Forwarded-Host: {}", request.getHeader("X-Forwarded-Host"));
+        log.info("X-Forwarded-Proto: {}", request.getHeader("X-Forwarded-Proto"));
+        log.info("Host: {}", request.getHeader("Host"));
+    }
+}
 }
