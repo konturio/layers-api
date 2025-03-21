ea1aa4cbe9361fe090f82ca73a4f0825c6321ac4
Andrew <33183728+frolui@users.noreply.github.com>
2025-03-21 11:36:19 +0100

    Update src/main/java/io/kontur/layers/config/MyBatisConfig.java
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/main/java/io/kontur/layers/config/MyBatisConfig.java
+++ src/main/java/io/kontur/layers/config/MyBatisConfig.java
@@ -1,11 +1,7 @@
 package io.kontur.layers.config;
 
-import io.kontur.layers.util.ListToJsonTypeHandler;
-import org.apache.ibatis.type.TypeHandlerRegistry;
 import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 @org.springframework.context.annotation.Configuration
 public class MyBatisConfig {
