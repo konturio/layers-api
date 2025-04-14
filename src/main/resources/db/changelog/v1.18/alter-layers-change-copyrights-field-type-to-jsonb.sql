01ffbdf8abf24cd5039dad4673c6e07968838cc8
avalasiuk <avalasiuk@kontur.io>
2025-04-14 14:29:28 +0200

    Add runInTransaction:false to v1.18 migration

--- src/main/resources/db/changelog/v1.18/alter-layers-change-copyrights-field-type-to-jsonb.sql
+++ src/main/resources/db/changelog/v1.18/alter-layers-change-copyrights-field-type-to-jsonb.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset layers-api-migrations:v1.18/alter-layers-change-copyrights-field-type-to-jsonb.sql runOnChange:false
+--changeset layers-api-migrations:v1.18/alter-layers-change-copyrights-field-type-to-jsonb.sql runOnChange:false runInTransaction:false
 
 ALTER TABLE layers 
 ALTER COLUMN copyrights SET DATA TYPE jsonb 
