4974d34f5389c88c2d97bb54b0de698c56f7aa2c
avalasiuk <avalasiuk@kontur.io>
2025-05-20 13:09:46 +0200

    Change SortOrder dto to use lowercase parameters

--- src/main/java/io/kontur/layers/dto/SortOrder.java
+++ src/main/java/io/kontur/layers/dto/SortOrder.java
@@ -1,6 +1,24 @@
 package io.kontur.layers.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum SortOrder {
     ASC,
     DESC;
+
+    @JsonCreator
+    public static SortOrder fromValue(String value) {
+        if (value == null) return ASC;
+        switch (value.toLowerCase()) {
+            case "asc": return ASC;
+            case "desc": return DESC;
+            default: throw new IllegalArgumentException("Invalid sort order: " + value);
+        }
+    }
+
+    @JsonValue
+    public String toValue() {
+        return name().toLowerCase(); // Возвращает "asc" или "desc"
+    }
 }
