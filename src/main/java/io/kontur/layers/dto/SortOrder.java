20d6cdaa2e065ceec94db55734c49459c8960397
avalasiuk <avalasiuk@kontur.io>
2025-05-20 13:41:25 +0200

    Add default value

--- src/main/java/io/kontur/layers/dto/SortOrder.java
+++ src/main/java/io/kontur/layers/dto/SortOrder.java
@@ -11,8 +11,9 @@ public enum SortOrder {
     public static SortOrder fromValue(String value) {
         if (value == null) return ASC;
         switch (value.toLowerCase()) {
-            case "asc": return ASC;
             case "desc": return DESC;
+            case "asc":
+            default: return ASC;
         }
     }
 
