2693eb09ecca14d9d9ba511de823c66c4b6c2ec1
Darafei Praliaskouski <me@komzpa.net>
2025-06-12 01:28:41 +0400

    Add index for fast layer feature retrieval

---
+++ src/main/resources/db/changelog/v1.19/create-index-layers-features-layer-id.sql
@@ -0,0 +1,5 @@
+--liquibase formatted sql
+
+--changeset layers-api-migrations:v1.19/create-index-layers-features-layer-id.sql runOnChange:true
+
+CREATE INDEX IF NOT EXISTS layers_features_layer_id_idx ON layers_features USING btree(layer_id);
