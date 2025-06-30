c8726b2f84be5916c856ad6ca989dd1c8e85f5f0
avalasiuk <avalasiuk@kontur.io>
2025-06-30 11:47:37 +0200

    Rename migration to fix layers-api release version

--- src/main/resources/db/changelog/v1.20/create-index-layers-features-layer-resolution.sql
+++ src/main/resources/db/changelog/v1.20/create-index-layers-features-layer-resolution.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset layers-api-migrations:v1.19/create-index-layers-features-layer-resolution.sql runOnChange:true
+--changeset layers-api-migrations:v1.20/create-index-layers-features-layer-resolution.sql runOnChange:true
 
 CREATE INDEX IF NOT EXISTS layers_features_layer_resolution_idx
 ON layers_features (
