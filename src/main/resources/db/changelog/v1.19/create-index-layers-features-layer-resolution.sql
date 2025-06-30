0a2b825aa205a2f05c39dffac11c52ed46d5a87a
avalasiuk <avalasiuk@kontur.io>
2025-06-30 18:46:42 +0200

    Rebase

--- src/main/resources/db/changelog/v1.19/create-index-layers-features-layer-resolution.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
-
---changeset layers-api-migrations:v1.19/create-index-layers-features-layer-resolution.sql runOnChange:true
-
-CREATE INDEX IF NOT EXISTS layers_features_layer_resolution_idx
-ON layers_features (
-  layer_id,
-  ((properties ->> 'resolution')::int)
-);
